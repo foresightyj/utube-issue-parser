ce34ebbb24992f913d48500f5df50e07ae525221
foresightyj <imyuanjian@gmail.com>
2020-10-23 00:27:58 +0800

    minor update

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,21 @@ async function run(): Promise<void> {
       core.warning("no issue, return");
       return;
     }
+    core.info(`issue html_url: ${issue.html_url}`);
     core.warning(`issue number: ${issue.number}`);
     core.warning(`issue body: ${issue.body}`);
+    core.warning(`issue: ${JSON.stringify(issue)}`);
+    const body = issue.body;
+    if (!body) {
+      core.warning("empty issue body, return");
+      return;
+    }
+    if (!body.includes("www.youtube.com/watch?v=")) {
+      core.warning("not youtube video, return");
+      return;
+    }
+    core.setOutput("url", body.trim());
     // const client = new github.GitHub(githubToken);
-    core.setOutput("issue body", issue.body);
   } catch (error) {
     core.setFailed(error.message);
   }
